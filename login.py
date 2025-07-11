5092d4287bed7575e2ca6fbb5105565edb65402e
Riddhimann Mukherjee <138844240+RM-0110@users.noreply.github.com>
2025-07-11 08:43:07 +0530

    Update login.py

--- login.py
+++ login.py
@@ -93,8 +93,8 @@ def signin(driver, username, password, mode_of_work):
         
         if signin_button:
             # random_time = random.randint(120,300)
-            print(f"Sleeping for {random_time} seconds...")
-            time.sleep(random_time)
+            # print(f"Sleeping for {random_time} seconds...")
+            # time.sleep(random_time)
             signin_button.click()
             print("Signin button clicked.")
         else:
